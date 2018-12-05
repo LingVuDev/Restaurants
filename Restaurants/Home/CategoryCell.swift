7190468885a4096fcdc0eb80875d8fa9ac8ad1ad
Ling Vu <vumanhling@icloud.com>
2018-12-05 07:09:20 +0100

    Added different cells on the featuredController

---
+++ Restaurants/Home/CategoryCell.swift
@@ -0,0 +1,73 @@
+//
+//  CategoryCell.swift
+//  Restaurants
+//
+//  Created by Ling on 05.12.18.
+//  Copyright © 2018 Ling. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    private let cellId = "cellId"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let featuresCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        return collectionView
+    }()
+    
+    func setupViews() {
+        backgroundColor = .clear
+        
+        addSubview(featuresCollectionView)
+        featuresCollectionView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+        featuresCollectionView.dataSource = self
+        featuresCollectionView.delegate = self
+    
+        featuresCollectionView.register(FeaturedCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as? FeaturedCell {
+            return cell
+        }
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: 150)
+    }
+}
+
+class FeaturedCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews() {
+        backgroundColor = .red
+    }
+}
---
+++ Restaurants/Home/FeaturedController.swift
@@ -0,0 +1,39 @@
+//
+//  HomeController.swift
+//  Restaurants
+//
+//  Created by Ling on 05.12.18.
+//  Copyright © 2018 Ling. All rights reserved.
+//
+
+import UIKit
+
+class FeaturedController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    private let cellId = "cellId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.register(CategoryCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.backgroundColor = .white
+        navigationItem.title = "Featured"
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as? CategoryCell {
+            return cell
+        }
+        return UICollectionViewCell()
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 150)
+    }
+    
+    
+}
