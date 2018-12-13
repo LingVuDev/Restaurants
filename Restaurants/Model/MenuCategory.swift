fd97daa055f919c70c8369885b507fcaa87380c0
Ling Vu <vumanhling@icloud.com>
2018-12-13 20:43:49 +0100

    .

---
+++ Restaurants/Model/MenuCategory.swift
@@ -0,0 +1,17 @@
+//
+//  Menu.swift
+//  Restaurants
+//
+//  Created by Ling Vu on 08.12.18.
+//  Copyright © 2018 Ling. All rights reserved.
+//
+
+import Foundation
+
+struct Menu {
+    let name: String
+    let category: String
+    let description: String
+    let dishes: [Dish]
+    
+}
---
+++ Restaurants/SingleDish/DishController.swift
@@ -0,0 +1,9 @@
+//
+//  DishController.swift
+//  Restaurants
+//
+//  Created by Ling Vu on 08.12.18.
+//  Copyright © 2018 Ling. All rights reserved.
+//
+
+import Foundation
